b3585013647838fa5f337ab44865b9d0b31fecaf
lixfelix123 <70250023+lixfelix123@users.noreply.github.com>
2021-10-03 11:00:03 +0700

    Update create-table.sql

--- backend/database/create-table.sql
+++ backend/database/create-table.sql
@@ -3,15 +3,17 @@ users (
 	user_id SERIAL PRIMARY KEY, 
 	user_name varchar(20), 
 	user_tier char(1), 
-	user_location boolean, 
-	top_category text
+	isjawa boolean, 
+	top_category varchar(20)
 );
+
 
 CREATE TABLE IF NOT EXISTS 
 coupons (
 	coupon_id SERIAL PRIMARY KEY, 
-	coupon_name TEXT,
-	coupon_category C,
+	coupon_name text,
+	coupon_category varchar(20),
+	coupon_potongan int,
 	start_date TIMESTAMP,
 	end_date TIMESTAMP,
 	);
